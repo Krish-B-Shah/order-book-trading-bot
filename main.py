b6795cac55ce7c362ed01e85ed32e40392e4fa9b
Krish Shah <shahkrish25062006@gmail.com>
2025-06-24 16:29:25 -0400

    added and drawing matplot graph to track bot profit and loss

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ from order_book import Order, OrderBook
 from strategy import MarketMakingStrategy
 from matplotlib import pyplot as plt
 
-episodes = list(range(1, 6))
-profits = [20, -5, 15, 10, -3]
+# episodes = list(range(1, 6))
+# profits = [20, -5, 15, 10, -3]
 
-plt.bar(episodes, profits)
-plt.xlabel('Round')
-plt.ylabel('P&L')
-plt.title('Profit and Loss Over Time')
-plt.axhline(0, color='black', linewidth=0.8)
-plt.show()
+# plt.bar(episodes, profits)
+# plt.xlabel('Round')
+# plt.ylabel('P&L')
+# plt.title('Profit and Loss Over Time')
+# plt.axhline(0, color='black', linewidth=0.8)
+# plt.show()
 
 
 
@@ -26,12 +26,19 @@ book.add_order(Order(order_id=book.next_order_id(), side="sell", price=101, quan
 book.add_order(Order(order_id=book.next_order_id(), side="buy", price=101, quantity=5, order_type="market", owner=bot))
 
 # Market making loop
-for _ in range(10):
+
+rounds  = []
+pnlHistory = []
+NUM_ROUNDS = int(input("Enter number of simulation rounds: "))
+for round_num in range(NUM_ROUNDS):
     bid, ask = book.get_best_bid_ask()
     bot_orders = bot.generate_orders(bid or 98, ask or 102)
     for order in bot_orders:
         book.add_order(order)
     book.match()
+    rounds.append(round_num)
+    pnlHistory.append(bot.pNL)
+
     print(f"P&L: {bot.pNL:.2f} | Cash: {bot.cash:.2f} | Inventory: {bot.inventory}")
     print("-" * 50)
 
@@ -45,6 +52,13 @@ for trade in book.trade_log:
 print("\nRemaining Order Book:")
 book.print_book()
 
+plt.plot(rounds, pnlHistory, marker='o')
+plt.title("Bot Profit and Loss Over Time")
+plt.xlabel("Round")
+plt.ylabel("P&L")
+plt.axhline(0, color='black', linestyle='--', linewidth=0.8)
+plt.grid(True)
+plt.show()
 
 # Uncomment the following lines to export all orders to a CSV file
 # book.export_all_orders("all_orders.csv")
